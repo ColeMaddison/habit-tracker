d853a4c8592e35c87f7cea04a83ab8000df0d213
dk <dimonas92@gmail.com>
2025-09-14 16:31:49 +0200

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,23 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-    rules: {
-      "indent": ["error", 4],
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
+        rules: {
+            "indent": ["error", 4],
+        },
     },
-  },
 ];
 
 export default eslintConfig;
